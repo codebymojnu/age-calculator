e9a39bf061c18e108fa6b4077833a6827d6dc09d
jsmojnu <learnwithmojnu@gmail.com>
2021-12-10 11:05:07 +0600

    initial commit

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+document.querySelector('#btn').addEventListener('click', function(){
+    const birthDate = document.querySelector('#birthDate').value;
+    const birthMonth = document.querySelector('#birthMonth').value;
+    const birthYear = document.querySelector('#birthYear').value;
+    const todayDate = document.querySelector('#todayDate').value;
+    const currentMonth = document.querySelector('#currentMonth').value;
+    const currentYear = document.querySelector('#currentYear').value;
+    userData(birthDate, birthMonth, birthYear, todayDate, currentMonth, currentYear);
+})
+
+function userData(birthDate, birthMonth, birthYear, todayDate, currentMonth, currentYear){
+    console.log(birthDate, birthMonth, birthYear, todayDate, currentMonth, currentYear);
+    if(birthDate > todayDate && birthMonth > currentMonth){
+        todayDate = parseInt(todayDate) + 31;
+        currentMonth = parseInt(currentMonth) + 11;
+        currentYear = parseInt(currentYear) - 1;
+    }
+    if(birthDate > todayDate){
+        todayDate = parseInt(todayDate) + 30;
+        birthMonth = parseInt(birthMonth) + 1;
+        console.log(todayDate);
+        console.log(birthMonth);
+    }
+    if(birthMonth > currentMonth){
+        currentMonth = parseInt(currentMonth) + 12;
+        currentYear = parseInt(currentYear) - 1;
+    }
+    subtraction(birthDate, birthMonth, birthYear, todayDate, currentMonth, currentYear);
+}
+
+function subtraction(birthDate, birthMonth, birthYear, todayDate, currentMonth, currentYear){
+    const day = todayDate - birthDate;
+    const month = currentMonth - birthMonth;
+    const year = currentYear - birthYear;
+    const para = document.createElement('p');
+    document.querySelector('#showAge').appendChild(para);
+    document.querySelector('p').innerHTML = `<span>Result</span><br/> ${year} years ${month} months ${day} days`
+}
